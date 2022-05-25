f30ec278f7a1922979a3a6da3c3dff0a97723b57
fallingrust <23722021@qq.com>
2022-05-25 21:48:14 +0800

    first commit

---
+++ RaspberryForDTH11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RaspberryForDTH11 // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RaspberryForFanControl/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RaspberryForFanControl // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {        
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
